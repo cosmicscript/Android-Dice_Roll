758d8fc8f3458e62c1272e2599a971466e4518bc
vignesh <vignesh.raghupathy@zohocorp.com>
2021-10-03 18:20:26 +0530

    sg

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,22 +13,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
+        val imageView : ImageView = findViewById(R.id.imageView)
         rollButton.setOnClickListener {
-            rollDice()
+            rollTheDice(6, imageView)
             Toast.makeText(this, "Dice rolled", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun rollDice() {
-        val firstDice = Dice(6)
-        val resultTextView = findViewById<TextView>(R.id.textView)
-        resultTextView.text = firstDice.roll().toString()
-
-        val dice = Dice(20)
-        val diceVal = dice.roll()
-
-        val findViewById = findViewById<TextView>(R.id.textView2)
-        findViewById.text = diceVal.toString()
+    private fun rollTheDice(sides: Int, imageView: ImageView) {
+        val firstDice = Dice(sides)
+        val diceVal = firstDice.roll()
+        val imageSrc = when (diceVal) {
+            1 -> (R.drawable.dice_1)
+            2 -> (R.drawable.dice_2)
+            3 -> (R.drawable.dice_3)
+            4 -> (R.drawable.dice_4)
+            5 -> (R.drawable.dice_5)
+            else -> (R.drawable.dice_6)
+        }
+        imageView.contentDescription = diceVal.toString()
+        imageView.setImageResource(imageSrc)
     }
 }
 
